8b9a19f25bf2e4cb69142cf5ee8eafddac36cbe0
Anshumaan <anshmn10@gmail.com>
2024-09-22 01:15:54 +0530

    com

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>SEX</h1>
+        <h1>Valorant</h1>
       </header>
     </div>
   );
